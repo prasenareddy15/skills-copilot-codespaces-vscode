5a4f3037b50e45bc3c0df36ac1f7bf6afac3feb4
prasenareddy15 <66769276+prasenareddy15@users.noreply.github.com>
2023-07-25 07:27:57 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,1 +1,22 @@
 // Create web server
+// Import the required modules
+const http = require('http');
+
+// Define the host and port for the server
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// Create the HTTP server
+const server = http.createServer((req, res) => {
+  // Set the HTTP response header with status code and content type
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  // Write the response message
+  res.end('Hello, World! This is a basic web server.');
+});
+
+// Start the server and listen on the specified port and host
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
